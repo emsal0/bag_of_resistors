a29850b31782710e6ca44e9e2575f49c6b1ffa16
Emmanuel Sales <absolutezero2a03@gmail.com>
2020-05-02 13:37:02 -0700

    code alternative (slower but more correct) algo

--- resistors.js
+++ resistors.js
@@ -20,6 +20,113 @@ function setHasFloat(set, tgt, digits) {
     return false;
 }
 
+function processSteps(leftSteps, rightSteps, op) {
+    let steps = [];
+    let lastLeftStep = leftSteps[leftSteps.length - 1];
+    let lastRightStep = rightSteps[rightSteps.length - 1];
+    if (leftSteps.length == 1 && rightSteps.length == 1) {
+        return [...leftSteps, ...rightSteps, op];
+    } else if (leftSteps.length > 1 && rightSteps.length > 1) {
+        if (lastLeftStep === lastRightStep && lastRightStep === op) {
+            return [...leftSteps.splice(0, leftSteps.length - 1),
+                    ...rightSteps.splice(0, rightSteps.length - 1), op];
+        } else {
+            return [leftSteps, rightSteps, op];
+        }
+    } else {
+        if (leftSteps.length == 1) {
+            if (lastRightStep === op) {
+                return [...leftSteps,
+                    ...(rightSteps.splice(0, rightSteps.length - 1)), op];
+            } else {
+                return [...leftSteps, rightSteps, op];
+            }
+        } else if (rightSteps.length == 1) {
+            if (lastLeftStep === op) {
+                return [...(leftSteps.splice(0, leftSteps.length - 1)),
+                    ...rightSteps, op];
+            } else {
+                return [leftSteps, ...rightSteps, op];
+            }
+        }
+    }
+}
+
+function subset_resistor_redux_main(res_vals, tgt, visitedSolns, digits) {
+
+    let queue = [];
+    queue.push(res_vals);
+
+    let convertResistance = r => parseFloat(r.toFixed(digits));
+    let approxEquals = (r,s) => convertResistance(r) == convertResistance(s);
+
+    while (queue.length > 0) {
+        let curVals = queue.splice(0,1)[0];
+        let n = curVals.length;
+        let values = curVals.map(item =>
+            parseFloat(convertResistance(item['value'])));
+        let key = values.slice(0).sort().join();
+        if (visitedSolns.has(key)) {
+            continue;
+        } else if (!!values.find(j => approxEquals(j,tgt))) {
+            let soln = curVals.find(x => approxEquals(x['value'], tgt));
+            return soln;
+        } else if (n == 1) {
+            continue;
+        }
+
+        for (let i = 0; i < n; i++) {
+            let left = curVals[i];
+            let localVals = curVals.slice(0);
+            localVals.splice(i, 1);
+            for (let j = i; j < localVals.length; j++) {
+                let right = localVals[j];
+                localVals.splice(j, 1);
+
+                let leftSteps = left['steps'].slice(0);
+                let rightSteps = right['steps'].slice(0);
+
+                let seriesSoln = {
+                    'value': left['value'] + right['value'],
+                    'steps': processSteps(leftSteps, rightSteps, '+')
+                }
+
+                leftSteps = left['steps'].slice(0);
+                rightSteps = right['steps'].slice(0);
+
+                let parallelSoln = {
+                    'value': 1/(1/left['value'] + 1/right['value']),
+                    'steps': processSteps(leftSteps, rightSteps, '||')
+                }
+
+                queue.push(localVals.concat(seriesSoln));
+                queue.push(localVals.concat(parallelSoln));
+
+                localVals.push(right);
+            }
+            localVals.push(left);
+        }
+        visitedSolns.add(key);
+    }
+
+    return false;
+}
+
+function subset_resistor_redux(res_vals, tgt) {
+
+    let resValsFormatted = res_vals.map(v => {
+        return {
+            'value': v,
+            'steps': [v]
+        };
+    });
+
+    let X = max_signif(res_vals) + 1;
+    //console.log(`X = ${X}`);
+    let visitedSolns = new Set();
+    return subset_resistor_redux_main(resValsFormatted, tgt, visitedSolns, X);
+}
+
 function subset_resistor(res_vals, tgt) {
     let n = res_vals.length
     let X = max_signif(res_vals) + 1;
